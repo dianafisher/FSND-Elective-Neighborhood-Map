42606ee8ea1d20f9d81740c326d72f86280af59e
Diana Fisher <diana.fisher@gmail.com>
2017-02-28 13:30:27 -0800

    Change datasource to SF Gov.

--- src/js/app.js
+++ src/js/app.js
@@ -1,126 +1,207 @@
 
-var initialCats = [
-    {
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: '',
-        nicknames: ['Cutie Pie', 'Sweetie Pie', 'Catty Cat Cat', 'Mr. Whiskers']
-    },
-    {
-        clickCount: 0,
-        name: 'Shadow',
-        imgSrc: 'img/1413379559_412a540d29_z.jpg',
-        imgAttribution: '',
-        nicknames: ['Shooby', 'Scooby']
-    },
-    {
-        clickCount: 0,
-        name: 'Scaredy',
-        imgSrc: 'img/22252709_010df3379e_z.jpg',
-        imgAttribution: '',
-        nicknames: ['Bashful']
-    },
-    {
-        clickCount: 0,
-        name: 'Tiger',
-        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
-        imgAttribution: '',
-        nicknames: ['Lion', 'Tyger']
-    },
-    {
-        clickCount: 0,
-        name: 'Stripey',
-        imgSrc: 'img/9648464288_2516b35537_z.jpg',
-        imgAttribution: '',
-        nicknames: ['Zzzzz']
-    }
-];
+// Construct the catalog query string
+// const endpoint = 'http://data.ct.gov/resource/9k2y-kqxn.json?organization_type=Public%20School%20Districts&$$app_token=CGxaHQoQlgQSev4zyUh5aR5J3';
+const endpoint = 'https://data.sfgov.org/resource/wwmu-gmzc.json';
+
+var data = [];
+var viewModel;
+
+
+get(endpoint).then(function(response) {
+    // console.log('Success!', response);
+    console.log('Success!');
+
+    data = JSON.parse(response);
+    // console.log(data);
+
+    viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+
+    // data.forEach(entry => {
+    //     console.log(entry);
+    // });
+
+}, function(error){
+    console.error("Failed!", error);
+});
+
+//http://api.themoviedb.org/3/search/movie?api_key=APIKEY&query=MOVIENAME
+get('https://api.themoviedb.org/3/search/movie?api_key=9c8b8a24a248fed2e25eb1f8d2f29d13&language=en-US&query=180&page=1&include_adult=false&year=2011').then(function(response) {
+    console.log('Success!', response);
+});
+
+
+// get(sf_endpoint).then(function(response) {
+//     console.log('Success!', response);
+//     // console.log('Success!');
+
+//     // data = JSON.parse(response);
+//     // // console.log(data);
+//     // data.forEach(entry => {
+//     //     console.log(entry.locations);
+//     //     // codeAddress(entry.locations);
+//     //     if (entry.locations === undefined) {
+//     //         // these statements execute
+//     //     } else {
+//     //         codeAddress(entry.locations);
+//     //     }
+//     // });
+
+// }, function(error){
+//     console.error("Failed!", error);
+// });
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var codeAddress = function(address) {
+    // var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status === 'OK') {
+        // map.setCenter(results[0].geometry.location);
+        // var marker = new google.maps.Marker({
+        //     map: map,
+        //     position: results[0].geometry.location
+        // });
+        console.log(results[0].geometry.location);
+      } else {
+        // alert('Geocode was not successful for the following reason: ' + status);
+        console.error('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  };
+
+var Place = function(data) {
+    var self = this;
+    // console.log('creating Place for ', data);
+    this.lat = ko.observable(data.location_1.latitude);
+    this.lng = ko.observable(data.location_1.longitude);
+    this.address = ko.observable(data.location_1.human_address);
+    var coords = new google.maps.LatLng(this.lat(), this.lng());
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map
+    });
+    this.marker = ko.observable(marker);
+    google.maps.event.addListener(marker, 'click', function(){
+        viewModel.markerClicked(self);
+    });
+
+    var content = '<div id="content">' +
+            '<div id="siteNotice">' + '</div>' +
+            '<h1 id="heading" class="heading">' + data.name + '</h1>' +
+            '</div>';
+
+    // var content = data.name;
+
+    this.contentString = ko.observable(content);
+
+};
+
+var Location = function(data) {
+    var self = this;
+    geocoder.geocode(
+        {'address': data.locations},
+        function(results, status){
+            if (status === 'OK') {
+                console.log(data);
+                console.log(results.length);
+                results.forEach(result => {
+                    console.log(result.formatted_address);
+                });
+                console.log(results[0].geometry.location);
+                console.log(results[0].formatted_address);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+                self.marker = ko.observable(marker);
+                google.maps.event.addListener(marker, 'click', function(){
+                    viewModel.markerClicked(self);
+                });
+
+                // Generate the content string
+                var content =   '<div id="content">' +
+                                    '<div id="siteNotice">' + '</div>' +
+                                    '<h1 id="heading" class="heading">' + data.title  + '</h1>' +
+                                '</div>';
+
+                self.contentString = ko.observable(content);
+
+            } else {
+                console.error('Geocode was not successful for the following reason: ' + status);
+            }
+
+        });
+};
 
 var ViewModel = function() {
     // store the outer 'this' which represents the ViewModel
     var self = this;
 
-    this.cats = ko.observableArray([]);
+    this.locations = ko.observableArray([]);
+    // console.log(data);
 
-    // create Cat objects
-    initialCats.forEach(function(catItem){
-        self.cats.push( new Cat(catItem) );
+    // Create a new Location object for each item in the array.
+    data.forEach(function(location){
+        self.locations.push( new Location(location) );
     });
 
-    // 'this' represents the binding context when increment counter is clicked.
-    this.currentCat = ko.observable( this.cats()[0] );
+    // handles a click on a map marker
+    this.markerClicked = function(location) {
+        console.log(location);
 
-    this.incrementCounter = function() {
-        // 'this' represents the ViewModel, which does not have a clickCount.
-        // this.clickCount(this.clickCount() + 1);
+        var marker = location.marker();
+        console.log(marker);
 
-        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-    };
+        // toggle the info window
+        infowindow = new google.maps.InfoWindow({
+            content: location.contentString()
+        });
+        infowindow.open(map, marker);
 
-    this.catClicked = function() {
-        console.log('ViewModel: cat clicked!');
-        console.log(this);
-        console.log(this.name);
-
-        // switch current cat to be the selected cat
-        console.log(self.currentCat);
-        self.currentCat(this);
-    };
-};
-
-var Cat = function(data) {
-    var self = this;
-    self.clickCount = ko.observable(data.clickCount);
-    self.name = ko.observable(data.name);
-    self.imgSrc = ko.observable(data.imgSrc);
-    self.imgAttribution = ko.observable(data.imgAttribution);
-    self.nicknames = ko.observableArray(data.nicknames);
-
-    self.title = ko.computed(function(){
-        var title;
-        var clicks = self.clickCount();
-        if (clicks < 10) {
-            title = 'Newborn';
-        } else if (clicks < 50) {
-            title = 'Infant';
-        } else if (clicks < 100) {
-            title = 'Child';
-        } else if (clicks < 200) {
-            title = 'Teen';
-        } else if (clicks < 500) {
-            title = 'Adult';
+        // toggle the bounce animation
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
         } else {
-            title = 'Ninja';
+            marker.setAnimation(google.maps.Animation.BOUNCE);
         }
-        return title;
-    });
-
-    // this.title = ko.computed(function(){
-    //     var title;
-    //     var clicks = this.clickCount();
-    //     if (clicks < 10) {
-    //         title = 'Newborn';
-    //     } else if (clicks < 50) {
-    //         title = 'Infant';
-    //     } else if (clicks < 100) {
-    //         title = 'Child';
-    //     } else if (clicks < 200) {
-    //         title = 'Teen';
-    //     } else if (clicks < 500) {
-    //         title = 'Adult';
-    //     } else {
-    //         title = Ninja;
-    //     }
-    //     return title;
-    // }, this);
-
-    /*
-    *   The second parameter to ko.computed (the bit where we passed 'this' in the above example)
-        defines the value of this when evaluating the computed observable. Without passing it in,
-        it would not have been possible to refer to this.firstName() or this.lastName().
-    */
-
+    };
 };
 
-ko.applyBindings(new ViewModel());
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status === 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
--- src/js/map.js
+++ src/js/map.js
@@ -1,27 +1,21 @@
 var map;
 
-var styles_1 = [{"featureType":"water","stylers":[{"color":"#19a0d8"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":6}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#e85113"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-40}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-20}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"road.highway","elementType":"labels.icon"},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"lightness":20},{"color":"#efe9e4"}]},{"featureType":"landscape.man_made","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"hue":"#11ff00"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"hue":"#4cff00"},{"saturation":58}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f0e4d3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-10}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"}]}];
-var styles_2 = [{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#89cada"}]}];
+// var styles_1 = [{"featureType":"water","stylers":[{"color":"#19a0d8"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":6}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#e85113"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-40}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-20}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"road.highway","elementType":"labels.icon"},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"lightness":20},{"color":"#efe9e4"}]},{"featureType":"landscape.man_made","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"hue":"#11ff00"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"hue":"#4cff00"},{"saturation":58}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f0e4d3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-10}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"}]}];
+var styles = [{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#89cada"}]}];
 
 function initMap() {
-  // Construct the catalog query string
-  url = 'http://data.ct.gov/resource/9k2y-kqxn.json?organization_type=Public%20School%20Districts&$$app_token=CGxaHQoQlgQSev4zyUh5aR5J3';
-
-  var center = new google.maps.LatLng(41.7656874,-72.680087);
+//@37.7494945,-122.2824239
+  var center = new google.maps.LatLng(37.7494945,-122.2824239);
+  geocoder = new google.maps.Geocoder();
 
   // Intialize our map
   var mapOptions = {
-    zoom: 8,
+    zoom: 12,
     center: center,
-    styles: styles_2
+    styles: styles
   };
 
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", requestListener);
-  oReq.open("GET", url);
-  oReq.send();
 }
 
 function toggleBounce(marker) {
@@ -61,14 +55,3 @@ function requestListener() {
     }
   }
 
-
-
-
-// var map;
-      // function initMap() {
-      //   map = new google.maps.Map(document.getElementById('map'), {
-      //     center: {lat: -34.397, lng: 150.644},
-      //     zoom: 8,
-      //     styles: [{"featureType":"water","stylers":[{"color":"#19a0d8"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":6}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#e85113"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-40}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-20}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"road.highway","elementType":"labels.icon"},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"lightness":20},{"color":"#efe9e4"}]},{"featureType":"landscape.man_made","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"hue":"#11ff00"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"hue":"#4cff00"},{"saturation":58}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f0e4d3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-10}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"}]}]
-      //   });
-      // }
